0c5d7457cf277c7063eb471bc94a2d9fe0d05f03
olehcambel <nickcambel5@gmail.com>
2020-06-01 19:51:17 +0300

    add e

--- e/main.go
+++ e/main.go
@@ -1,59 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
 // Merge2Channels reads from channels, makes op and writes to out channel.
 func Merge2Channels(f func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
-	for i := 0; i < n; i++ {
-		x1, x2 := <-in1, <-in2
-		out <- f(x1) + f(x2)
-	}
-}
-
-func op(x int) int {
-	time.Sleep(time.Second * 2)
-	return x
-}
-
-func main() {
-	n := 10
-	in1 := make(chan int, n)
-	in2 := make(chan int, n)
-	out := make(chan int, n)
-
-	for i := 0; i < n; i++ {
-		in1 <- i * 10
-		in2 <- i + 1
+	work := func(x int, task func(int) int) chan int {
+		res := make(chan int, 1)
+		go func() {
+			res <- task(x)
+		}()
+		return res
 	}
 
-	go Merge2Channels(op, in1, in2, out, n)
+	go func() {
+		for i := 0; i < n; i++ {
+			x1, x2 := <-in1, <-in2
+			w1, w2 := work(x1, f), work(x2, f)
 
-	for i := 0; i < n; i++ {
-		fmt.Println(<-out)
-	}
-}
-
-func groups() {
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	work := func(id int) {
-		defer wg.Done()
-		fmt.Printf("Горутина %d начала выполнение \n", id)
-		time.Sleep(2 * time.Second)
-		fmt.Printf("Горутина %d завершила выполнение \n", id)
-	}
-
-	go work(1)
-	go work(2)
-	go work(3)
-
-	wg.Wait()
-	wg.Wait()
-
-	fmt.Print("done")
+			// while waiting for w1, w2 is running in goroutine (non-blocking) and vise versa
+			out <- <-w1 + <-w2
+		}
+	}()
 }
